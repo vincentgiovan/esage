276af9bdd731d99ef16ea4afecd89c4fd4c7f0ff
vincentgiovan <vincent.giovan@binus.ac.id>
2024-08-23 20:24:55 +0700

    123

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -29,7 +29,7 @@
                                 </button>
                             </div>
                         </form>
-<br>
+                        <br>
                         <form method="POST" action="{{ route('logout') }}">
                             @csrf
 
