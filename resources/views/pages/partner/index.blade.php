7dedd2e67ce213117e70dcdc2280f4626a47418b
Vannes Theo S <vannestheo@gmail.com>
2025-01-22 21:24:53 +0700

    minor fix

--- resources/views/pages/partner/index.blade.php
+++ resources/views/pages/partner/index.blade.php
@@ -99,7 +99,6 @@
                                     </form>
                                 </div>
                             </td>
-                        @endauth
                     </tr>
                 @endforeach
             </table>
